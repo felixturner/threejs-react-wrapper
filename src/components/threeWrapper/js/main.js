afb1c4d99a4f13cdf572680d48b272f43bfeb51c
felix turner <felixturner@gmail.com>
2021-04-29 19:04:14 -0700

    move assets folder up

--- src/components/threeWrapper/js/main.js
+++ src/components/threeWrapper/js/main.js
@@ -2,7 +2,7 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import Stats from 'three/examples/jsm/libs/stats.module.js';
 
-import testcard from './assets/testcard.jpg';
+import testcard from '../assets/testcard.jpg';
 
 /*
 
